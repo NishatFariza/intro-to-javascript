bc80626ae348f3463f1824103cad9c8d83f08884
NishatFariza <nishatfarzia78@gmail.com>
2022-01-26 19:13:11 +0600

    math four operator practice done

---
+++ math.js
@@ -0,0 +1,25 @@
+// + operator
+var eggPrice=43;
+var onionPrice=42;
+var totalPrice=eggPrice + onionPrice;
+// console.log(eggPrice+onionPrice);
+console.log(totalPrice);
+
+
+// - operator
+var onionPrice=43;
+var eggPrice=42;
+var priceDifference=onionPrice - eggPrice;
+console.log(priceDifference);
+
+// * operator
+var eggPrice=10;
+var eggQuantity=7;
+var eggPriceMultiplication=eggPrice * eggQuantity;
+console.log(eggPriceMultiplication);
+
+// / operator
+var monyAvilable=100;
+var applePrice=10;
+var appleQuantity=monyAvilable / applePrice;
+console.log(appleQuantity);
---
+++ new.js
@@ -0,0 +1,6 @@
+// + operator
+var eggPrice=43;
+var onionPrice=42;
+var totalPrice=eggPrice + onionPrice;
+// console.log(eggPrice+onionPrice);
+console.log(totalPrice);
