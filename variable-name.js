d76b606dc811bbe0f82af9a342df6743b4dc317f
NishatFariza <nishatfarzia78@gmail.com>
2022-01-26 16:11:07 +0600

    js variable practice add

---
+++ variable-name.js
@@ -0,0 +1,15 @@
+var name='Nishat Fariza';
+// variable not allow 
+var false =15;
+var true =15;
+var delete ='delete';
+var user name ='Nishat';
+var USER ='Nishat';
+var "user"="Nishat";
+var user-name='Nishat';
+var 12user='Nishat';
+
+// variable name allowed //
+var user ='Nishat';
+var user_name='Nishat'; //Tobe eyta paython ey beshi use hoi js ey noi//
+var userName='Nishat';
